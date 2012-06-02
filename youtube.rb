9c19e322092ba8a93b9f00302de47cf8ac0f2d33
Gaurav Chandrashekar <cggaurav@gmail.com>
2012-06-02 23:37:43 +0530

    keypress on the move

--- youtube.rb
+++ youtube.rb
@@ -2,4 +2,8 @@ require 'youtube_it'
 client = YouTubeIt::Client.new
 client = YouTubeIt::Client.new(:dev_key => "AI39si5vjEPx1dHjzUYszA5Tvcig784fCTTGvSRI2fbM8KR8gtP4vrFdM6AGtdZLZ-yLXV3bVus_vcLbCvlIuH6gHuqFhsm22Q")
 
-puts client.videos_by(:query => "penguin").inspect
+client.videos_by(:query => "penguin", :user => 'TEDxtalks').videos.each do |i|
+	puts i.unique_id
+	puts i.description
+	puts i.title
+end
